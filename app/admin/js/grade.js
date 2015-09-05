ee0af3652c9b5aa7461d91d981ce191f59452f9e
Praveen Kumar Pendyala <praveendath92@gmail.com>
2015-09-06 00:27:51 +0200

    Grades management added to admin app

--- app/admin/js/grade.js
+++ app/admin/js/grade.js
@@ -3,15 +3,26 @@
  *
  * For functions related to site administration
  */
-angular.module('AdminApp').controller('AdminCtrl', function($scope, config, $http, $cookies) {
-    $scope.students = [];
+angular.module('AdminApp').controller('GradeCtrl', function($scope, config, $http, $cookies) {
+    $scope.grades = [];
     $scope.response = '';
-    $scope.selected = null;
-    $scope.listStudents = function(){
-        console.log('Got it');
+    $scope.select = {
+        value: null,
+        grade: null,
+        changeSelect : function(){
+            for(i=0; i<$scope.grades.length; i++){
+                if($scope.grades[i].gradeid == $scope.select.value){
+                    $scope.select.grade = $scope.grades[i];
+                    break;
+                }
+            }
+        }
+    };
+
+    $scope.listAutoGrades = function(){
         var req = {
             method: 'GET',
-            url: config.apiUrl + '/admin/list/inactive?token=' + $cookies.token
+            url: config.apiUrl + '/admin/grade/list/unverified?token=' + $cookies.token
         };
         $scope.response = '';
         $http(req)
@@ -19,7 +30,7 @@ angular.module('AdminApp').controller('AdminCtrl', function($scope, config, $htt
             function(response){ // Success callback
                 $data = response.data;
                 if($data.responsecode==200)
-                    $scope.students = $data.students;
+                    $scope.grades = $data.autogrades;
                 $scope.response = $data.message;
             },
             function(response){ //Error callback
@@ -27,21 +38,24 @@ angular.module('AdminApp').controller('AdminCtrl', function($scope, config, $htt
             }
         );
     };
-    $scope.activate = function(studentid){
+
+    $scope.verify = function(gradeid){
         var req = {
             method: 'GET',
-            url: config.apiUrl + '/admin/activate?token=' + $cookies.token
-            + "&studentid=" + studentid
+            url: config.apiUrl + '/admin/grade/verify?token=' + $cookies.token
+            + "&gradeid=" + gradeid
         };
+        console.log(req.url);
         $scope.response = '';
         $http(req)
             .then(
             function(response){ // Success callback
                 $data = response.data;
-                console.log($data);
-                if($data.responsecode == 200)
-                    $scope.students = $data.students;
                 $scope.response = $data.message;
+
+                $scope.listAutoGrades();
+                $scope.select.value = null;
+                $scope.select.grade = null;
             },
             function(response){ //Error callback
                 $scope.response = response.toString();
